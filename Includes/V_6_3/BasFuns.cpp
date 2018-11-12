8dea0b426b04e2e0be8717b7792b24da90bbff5e
Gigantor2131 <44032190+Gigantor2131@users.noreply.github.com>
2018-11-12 16:42:53 -0700

    file split

---
+++ Includes/V_6_3/BasFuns.cpp
@@ -0,0 +1,107 @@
+int ControllerScreen() {
+    while(1){
+        if(DriveMotorInverted!=DriveMotorInvertedWas){
+           // Controller1.Screen.newLine();
+            if(DriveMotorInverted) Controller1.Screen.print("Fliper");
+            else                   Controller1.Screen.print("Baller");
+        }
+        DriveMotorInvertedWas=DriveMotorInverted;
+        vex::task::sleep(50);
+    }
+}
+void BallFeedVars(){
+    Brain.Screen.newLine();
+    Brain.Screen.print(PuncSen.value(vex::analogUnits::pct));
+    Brain.Screen.print(" , ");
+    Brain.Screen.print(FeedSen1.value(vex::analogUnits::pct));
+    Brain.Screen.print(" , ");
+    Brain.Screen.print(FeedSen2.value(vex::analogUnits::pct));
+}
+int BrainScreen(){
+    while(1){
+        BallFeedVars();
+        Brain.Screen.render();
+        vex::task::sleep(20);
+    }
+}
+int ComRumer(){
+    ComRumerEnabled=true;
+    while(ComRumerEnabled){
+        if(ComRum && IntakeAutoEnabled && IntakeSetting==Intake(STOP))  Controller1.rumble(".");
+        vex::task::sleep(20);
+    }
+    return 1;
+}
+void LiftStop(){
+    LiftMotor.stop();
+}
+void LiftSMS(int Pct){
+    if(Pct==0)  LiftStop();
+    else{
+        LiftMotor.spin(vex::directionType::fwd,Pct,vex::velocityUnits::pct);
+    }
+}
+//
+void PuncherStop(){
+    PuncherMotor.stop();
+}
+void PuncherSMS(int Pct){
+    if(Pct==0)  PuncherStop();
+    else{
+        PuncherMotor.spin(vex::directionType::fwd,Pct,vex::velocityUnits::pct);
+    }
+}
+void IntakeStop(){
+    IntakeMotor.stop();
+}
+void IntakeSMS(int Pct){
+    if(Pct==0)  IntakeStop();
+    else{
+        IntakeMotor.spin(vex::directionType::fwd,Pct,vex::velocityUnits::pct);
+    }
+}
+void FliperStop(){
+    FlipMotor.stop();
+}
+void FliperSMS(int Pct){
+    if(FlipMotor.rotation(vex::rotationUnits::deg)>Fliper(UP) && Pct>0)     Pct=0;//upper limit
+    if(FlipMotor.rotation(vex::rotationUnits::deg)<Fliper(DOWN) && Pct<0)   Pct=0;//lower limit
+    if(Pct==0)  FliperStop();
+    else{
+        FlipMotor.spin(vex::directionType::fwd,Pct,vex::velocityUnits::pct);
+    }
+}
+//
+void DriveTimeOutSet(int TimeOut){
+    FLDriveMotor.setTimeout(TimeOut,vex::timeUnits::msec);
+    FRDriveMotor.setTimeout(TimeOut,vex::timeUnits::msec);
+    BRDriveMotor.setTimeout(TimeOut,vex::timeUnits::msec);
+    BLDriveMotor.setTimeout(TimeOut,vex::timeUnits::msec);
+}
+void LeftDriveStop(){
+    FLDriveMotor.stop();
+    BLDriveMotor.stop();
+}
+void RightDriveStop(){
+    FRDriveMotor.stop();
+    BRDriveMotor.stop();
+}
+void LeftDriveSMS(int pct){
+    if(pct==0)   LeftDriveStop();
+    else{
+        FLDriveMotor.spin(vex::directionType::fwd,pct,vex::velocityUnits::pct);
+        BLDriveMotor.spin(vex::directionType::fwd,pct,vex::velocityUnits::pct);
+    }
+}
+void RightDriveSMS(int pct){
+    if(pct==0)  RightDriveStop();
+    else{
+        FRDriveMotor.spin(vex::directionType::fwd,pct,vex::velocityUnits::pct);
+        BRDriveMotor.spin(vex::directionType::fwd,pct,vex::velocityUnits::pct);
+    }
+}
+void DriveSMS(int left, int right){
+    LeftDriveSMS(left);
+    RightDriveSMS(right);
+}
+//
---
+++ Includes/V_6_3/UsrFuns.cpp
@@ -0,0 +1,215 @@
+void LiftControl(){
+    if(Controller1.ButtonRight.pressing())      LiftSMS(100);
+    else if(Controller1.ButtonLeft.pressing())  LiftSMS(-100);
+    else                                        LiftSMS(0);
+}
+//
+//rstet timer if ball in puncher or
+void IntakeAutoUpDate(){//UpDate Sensors Code
+    //Puncher UpDate
+    if(PuncSen.value(vex::analogUnits::pct)<PuncBallTal){
+        GlobTime=0;//reset timer
+        PuncBall=true;
+        ComRum=false;
+    }  
+    else{
+        if(PuncherControlEnabled)           PuncBall=false;
+        else{
+            if(GlobTime>PuncBallTimeWait)   PuncBall=false;
+            else if(GlobTime>ComRumTime)    ComRum=true;
+            GlobTime=GlobTime+1;//add one to timer
+        } 
+    }
+    //FeedBall UpDate
+    if(FeedSen1.value(vex::analogUnits::pct)<Feed1BallTal || FeedSen2.value(vex::analogUnits::pct)<Feed2BallTal)    FeedBall=true;
+    else                                                                                                            FeedBall=false;
+}
+void IntakeAuto(){//Autonomous Logic Control
+    if(IntakeAutoEnabled){
+        IntakeAutoEnabledWas=true;
+        if(!PuncBall)       IntakeSetting=Intake(IN);
+        else{
+            if(!FeedBall)   IntakeSetting=Intake(IN);
+            else            IntakeSetting=Intake(STOP);
+        }
+    }
+    else if(IntakeAutoEnabledWas){//first loop disabled
+        IntakeSetting=Intake(STOP);
+        IntakeAutoEnabledWas=false;
+    }
+}
+int IntakeStateUpDate(){//Task to UpDate IntakeAutoUpDate every second in the background
+    while(1){
+        IntakeAutoUpDate();
+        vex::task::sleep(5);
+    }
+}
+void IntakeAutoControl(){//Controller Input To control Autonomous Logic Control
+    if(Controller1.ButtonA.pressing() && !APressed){
+        APressed=true;
+        IntakeAutoEnabled=!IntakeAutoEnabled;
+    }
+    else if(!Controller1.ButtonA.pressing() && APressed)    APressed=false;
+
+    IntakeAuto();
+}
+void IntakeManualControl(){//Controller Manual OverRide
+    if(Controller1.ButtonR2.pressing()){
+        IntakeManualControlEnabled=true;
+        IntakeSetting=Intake(OUT);
+    }
+    else if(Controller1.ButtonR1.pressing()){
+        IntakeManualControlEnabled=true;   
+        IntakeSetting=Intake(IN);
+
+    }
+    else if(IntakeManualControlEnabled){//first loop disabled
+        IntakeAutoEnabled=false;
+        IntakeManualControlEnabled=false;
+        IntakeSetting=Intake(STOP);
+    }
+}
+void IntakeControl(){//OverRide Control Code
+    IntakeManualControl();
+    if(!IntakeManualControlEnabled) IntakeAutoControl();
+    IntakeSMS(IntakeSetting);
+}
+//
+/*
+int PuncherCal(){
+    PuncherMotor.setTimeout(10,vex::timeUnits::msec);
+    PuncherMotor.spin(vex::directionType::rev,100,vex::velocityUnits::pct);
+    while(PMIS){}
+    PuncherMotor.setTimeout(250,vex::timeUnits::msec);
+    PuncherMotor.resetRotation();
+    PuncherMotor.startRotateFor(300,vex::rotationUnits::deg,100,vex::velocityUnits::pct);    
+    return 1;
+}
+*/
+void PuncherControl(){
+    if(Controller1.ButtonX.pressing()){
+        PuncherControlEnabled=true;
+        IntakeSetting=Intake(STOP);
+        if(FliperRequested==Fliper(UP)) FliperRequested=Fliper(MID);
+        PuncherSMS(100);
+    }
+    else if(PuncherControlEnabled){//first loop not enabled
+        PuncherSMS(0);
+        if(FliperRequested==Fliper(MID))    FliperRequested=Fliper(UP);
+        IntakeTimeEnabled=true;
+        PuncherControlEnabled=false;
+    }
+}
+//
+
+int FliperCal(){
+    FlipMotor.setTimeout(1,vex::timeUnits::msec);
+    FlipMotor.spin(vex::directionType::fwd,100,vex::velocityUnits::pct);
+    while(FlipMotor.isSpinning()){}
+    FlipMotor.setTimeout(250,vex::timeUnits::msec);
+    FlipMotor.resetRotation();
+    return 1;
+}
+void FliperManualControl(){
+    if(Controller1.ButtonRight.pressing()){
+        FliperManualControlEnabled=true;
+        FliperSMS(100);  
+    }
+    else if(Controller1.ButtonLeft.pressing()){
+        FliperManualControlEnabled=true;
+        FliperSMS(-100);
+    }
+    else if(FliperManualControlEnabled){//first loop
+        FliperSMS(0);
+        FliperManualControlEnabled=false;
+    }
+}
+void FliperFlip(){
+    if(FliperRequested==Fliper(UP)) FliperRequested=Fliper(DOWN);
+    else if(FliperRequested==Fliper(MID))   FliperRequested=Fliper(DOWN);
+    else if(FliperRequested==Fliper(DOWN))  FliperRequested=Fliper(UP);
+}
+void FliperPosControl(){
+    if(Controller1.ButtonL1.pressing() && !L1Pressed){
+        L1Pressed=true;
+        FliperFlip();
+        FliperPosControlEnabled=true;
+    }
+    if(!Controller1.ButtonL1.pressing() && L1Pressed)   L1Pressed=false;
+
+    if(FliperPosControlEnabled){
+        FlipMotor.startRotateTo(FliperRequested,vex::rotationUnits::deg,100,vex::velocityUnits::pct);
+    }
+}
+void FliperControl(){
+    //FliperManualControl();
+    //if(!FliperManualControlEnabled) FliperPosControl();
+    FliperPosControl();
+}
+//
+void DriveHoldControl(){
+    if(Controller1.ButtonL2.pressing() && !L2Pressed){
+        L2Pressed=true;
+        DriveBrake=!DriveBrake;
+    }
+    else if(!Controller1.ButtonL2.pressing() && L2Pressed)   L2Pressed=false;
+
+    if(DriveBrake){
+        FLDriveMotor.setStopping(vex::brakeType::hold);
+        FRDriveMotor.setStopping(vex::brakeType::hold);
+        BLDriveMotor.setStopping(vex::brakeType::hold);
+        BRDriveMotor.setStopping(vex::brakeType::hold);
+    }
+    else{
+        FLDriveMotor.setStopping(vex::brakeType::coast);
+        FRDriveMotor.setStopping(vex::brakeType::coast);
+        BLDriveMotor.setStopping(vex::brakeType::coast);
+        BRDriveMotor.setStopping(vex::brakeType::coast); 
+    }
+}
+int LD=0;
+int RD=0;
+void DriveUsrRequest(int L,int R){
+    LD=L;
+    RD=R;
+}
+void DriveManualControl(){
+    if(Controller1.ButtonUp.pressing() && !DriveInvertConBtnPressed){
+        DriveInvertConBtnPressed=true;
+        DriveMotorInverted=!DriveMotorInverted;
+    }
+    else if(!Controller1.ButtonUp.pressing() && DriveInvertConBtnPressed){
+        DriveInvertConBtnPressed=false;
+    }
+    LJoy=Controller1.Axis3.value();
+    RJoy=Controller1.Axis2.value();
+
+    if(LJoy!=0 || RJoy!=0){
+        DriveManualControlEnabled=true;
+        DriveUsrRequest(DriveMotorInverted ? -RJoy : LJoy,DriveMotorInverted ? -LJoy : RJoy);
+    }
+    else{
+        if(DriveManualControlEnabled)  DriveUsrRequest(0,0);//Last loop before disableing; used to release drivemanualcontrol
+        DriveManualControlEnabled=false;
+    }
+}
+bool Drive2ManualControlEnabled=false;
+void SecDriver(){
+    LJoy=Controller2.Axis3.value();
+    RJoy=Controller2.Axis2.value();
+
+    if(LJoy!=0 || RJoy!=0){
+        Drive2ManualControlEnabled=true;
+        DriveUsrRequest(-RJoy,-LJoy);
+    }
+    else{
+        if(Drive2ManualControlEnabled)  DriveUsrRequest(0,0);//Last loop before disableing; used to release drivemanualcontrol
+        Drive2ManualControlEnabled=false;
+    }
+}
+void DriveControl(){
+    DriveManualControl();
+    DriveHoldControl();
+    if(!DriveManualControlEnabled)  SecDriver();
+    DriveSMS(LD,RD);
+}
