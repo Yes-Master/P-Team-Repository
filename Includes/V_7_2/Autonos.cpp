9f32b7cfd26fd10907cbcdf7116dccba7abfd790
Gigantor2131 <44032190+Gigantor2131@users.noreply.github.com>
2018-12-05 19:09:09 -0700

    front autonos working

--- Includes/V_7_2/Autonos.cpp
+++ Includes/V_7_2/Autonos.cpp
@@ -1,16 +1,16 @@
 void AutFunFrontRed(){
     Drive(24,100,-1);
-    Drive(3,50,250);
-    Drive(-26,100,-1);
-    DriveRecon(-25,500);
+    Drive(3,50,500);
+    Drive(-27,100,-1);
+    DriveRecon(-35,500);
 
-    Drive(2.5,50);
+    Drive(3,50);
     Turn(-84);
     Drive(1,25);
     Puncher();
-    Drive(11,100);
+    Drive(10,100);
     if(PuncBall)    Puncher();
-    Turn(-3);//slight turn to miss pole
+    Turn(-6);//slight turn to miss pole
     // IntakeAutoEnabled=false;
     Drive(2,100);
 
@@ -19,30 +19,30 @@ void AutFunFrontRed(){
     Drive(-8,100,0);
     // IntakeAutoEnabled=true;
     Drive(-15,25,750);
-    Flip(FliperPosUpMid);
+    Flip(FliperPosIn);
 }
 void AutFunFrontBlue(){
     Drive(24,100,-1);
-    Drive(3,50,250);
-    Drive(-26,100,-1);
-    DriveRecon(-25,500);
+    Drive(3,50,500);
+    Drive(-27,100,-1);
+    DriveRecon(-35,500);
 
-    Drive(2.5,50);
-    Turn(90);
+    Drive(3,50);
+    Turn(85);
     Drive(1,25);
     Puncher();
-    Drive(11,100);
+    Drive(12,100);
     if(PuncBall)    Puncher();
-    Turn(3);//slight turn to miss pole
+    Turn(5);//slight turn to miss pole
     // IntakeAutoEnabled=false;
-    Drive(2,100);
+    Drive(3,100);
 
-    Turn(30);//45deg turn get cap
+    Turn(25);//45deg turn get cap
     Flip(FliperPosDown);
     Drive(-8,100,0);
     // IntakeAutoEnabled=true;
     Drive(-15,25,750);
-    Flip(FliperPosUpMid);
+    Flip(FliperPosIn);
 }
 void AutFunBackRed(){
     Drive(20,100,-1);
@@ -87,17 +87,17 @@ void AutoSelRunFuns(){
     }
     else if(AutSel1.value(vex::analogUnits::pct)>50){
         if(AutoProg==ProgPot::Pink)         FrontSkills();//Skills
-        else if(AutoProg==ProgPot::Green)   AutFunBackRed();//Normal
+        else if(AutoProg==ProgPot::Green)   AutFunFrontRed();//Normal
         else if(AutoProg==ProgPot::Yellow)  Sniper();//Sniper
     }
     else if(AutSel1.value(vex::analogUnits::pct)>17){
         if(AutoProg==ProgPot::Pink)         Testing();//Skills
-        else if(AutoProg==ProgPot::Green)   AutFunBackRed();//Normal
+        else if(AutoProg==ProgPot::Green)   AutFunFrontBlue();//Normal
         else if(AutoProg==ProgPot::Yellow)  Sniper();//Sniper
     }
     else{
         if(AutoProg==ProgPot::Pink)         Testing();//Skills
-        else if(AutoProg==ProgPot::Green)   AutFunBackRed();//Normal
+        else if(AutoProg==ProgPot::Green)   AutFunBackBlue();//Normal
         else if(AutoProg==ProgPot::Yellow)  Sniper();//Sniper
     }
 }
