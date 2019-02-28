4aa9bfdf42d9d5e8b23ae97b6ec5ef7400ba0447
Gigantor2131 <44032190+Gigantor2131@users.noreply.github.com>
2019-02-28 16:28:26 -0700

    Update ConFuns.cpp

--- share/ConFuns.cpp
+++ share/ConFuns.cpp
@@ -46,7 +46,7 @@ void PreLogic(){
     Clamp.SetTolerance(1);
 
     Lift.SetStop(vex::brakeType::hold);
-    Lift.SetPosLimits(10,800);
+    Lift.SetPosLimits(10,450);
     // Lift.SetTolerance(1);
     // Lift.SetVSetting(0);
     // Lift.SendControl=SendControl::SST;//start user in sst mode
