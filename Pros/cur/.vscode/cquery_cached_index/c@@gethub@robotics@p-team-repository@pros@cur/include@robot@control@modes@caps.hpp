14be1fa3b44b94babf10b6c48195e5e51c9ae551
Gigantor2131 <44032190+Gigantor2131@users.noreply.github.com>
2019-04-06 02:28:13 -0600

    remove2

--- Pros/cur/.vscode/cquery_cached_index/c@@gethub@robotics@p-team-repository@pros@cur/include@robot@control@modes@caps.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef CAPS_HPP
-#define CAPS_HPP
-namespace caps{
-  void user();
-  void init();
-  void deInit();
-}
-#endif /* end of include guard: CAPS_HPP */
--- Pros/cur/.vscode/cquery_cached_index/c@@gethub@robotics@p-team-repository@pros@cur/include@robot@control@modes@flag.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef FLAG_HPP
-#define FLAG_HPP
-namespace flag{
-  void init();
-  void user();
-  void deInit();
-}
-#endif /* end of include guard: FLAG_HPP */
